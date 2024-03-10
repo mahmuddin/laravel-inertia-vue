fd22160fefe47107153070c5ee899c441b609cdc
Mahmuddin Nurul Fajri <mahmuddinnf@gmail.com>
2024-03-10 08:16:15 +0700

    [main] 10 Mart 2024 - Index view

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container mx-auto pt-5">
+        <div class="post__block" v-for="post in posts" :key="post.id">
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.author }}</p>
+            <Link :href="`/post/${post.id}`">Read more</Link>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+    posts: Object
+});
+</script>
